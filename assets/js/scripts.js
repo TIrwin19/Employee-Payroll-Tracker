6454847c2bc59062faa9f437bdf4bb2b5d5d91cb
Blackbox19 <trevorirwin819@gmail.com>
2024-03-23 16:41:04 -0400

    finished third function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -67,7 +67,11 @@ const displayAverageSalary = function (employeesArray) {
 // Select a random employee
 const getRandomEmployee = function (employeesArray) {
   // TODO: Select and display a random employee
- 
+ // Math.floor
+  // Math.random
+  const ranIndex = Math.floor(Math.random()*employeesArray.length)
+  const ranEmployee = employeesArray[ranIndex]
+  console.log(`Your random eployee is: ${ranEmployee.firstName} ${ranEmployee.lastName}`)
 }
 
 /*
